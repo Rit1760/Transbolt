d07ee25c4c09d9d9dc9a9804a9eb7093fae2ac05
Your GitHub Username <ritikattri043@gmail.com>
2026-01-07 15:22:14 +0530

    update message

--- src/app/AES/page.jsx
+++ src/app/AES/page.jsx
@@ -8,8 +8,8 @@ export default function AES(){
       <div className="container1_AES">
         <div className="row1_AES">
           <div className="col1_AES">
-            <h2><span style={{color:"#d6d007ff"}}> NIVARAK </span>Comprehensive Lightning Prevention System</h2>
-            <h3>Powered by CMCE Technology</h3>
+            <h2><span style={{color:"#c1bb06ff"}}> NIVARAK </span>Comprehensive Lightning Prevention System</h2>
+            <h3>Powered by CMCE Technology by SERTEC</h3>
             <p> Certified Protection You Can Trust
  Our UL-Certified Lightning Prevention System
  offers state-of-the-art electrostatic field
